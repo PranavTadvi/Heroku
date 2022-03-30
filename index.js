9f44540baa863bb2f1434b7f7c8092e6e14d0307
PranavTadvi <p.d.tadvi8825@gmail.com>
2022-03-30 13:53:00 +0530

    new server changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-const port = process.env.port || 5000;
+const port = Process.env.PORT || 5000;
 app.get("/",(req,res)=>{
   
     res.send("Heroku Testing!!!")
