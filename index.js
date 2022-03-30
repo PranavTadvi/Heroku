38655e73dde5c553388efe5ba212c77afe30e470
PranavTadvi <p.d.tadvi8825@gmail.com>
2022-03-30 13:50:59 +0530

    server changes

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require("express");
 
 const app = express();
 
-
+const port = process.env.port || 5000;
 app.get("/",(req,res)=>{
   
     res.send("Heroku Testing!!!")
 })
 
-app.listen(5000,(req,res)=>{
+app.listen(port,(req,res)=>{
     console.log("server is running.....");
 })
